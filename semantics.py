bd6816ab978e15630f024084e07d46b6098bb754
Justin Mealey <justinmealey16@gmail.com>
2025-09-20 11:56:09 -0700

    Merged versions, semantic not working

--- semantics.py
+++ semantics.py
@@ -1,2 +1,16 @@
+import numpy as np
+from langchain_huggingface import HuggingFaceEmbeddings
+
+emb = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
 def compute_semantic_similarity(str1, str2):
-    return 1
+    ea = np.array(emb.embed_query(str1))
+    eb = np.array(emb.embed_query(str2))
+    return abs(cosine(ea, eb))
+
+
+def cosine(a, b):
+    a = a / np.linalg.norm(a)
+    b = b / np.linalg.norm(b)
+    return float(np.dot(a, b))
